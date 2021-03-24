0506262b111ed9e04114aed904b0630141b968a5
xbtshady <943217258@qq.com>
2021-03-24 23:32:49 +0800

    提交作业

---
+++ no4/user-platform/my-dependency-injection/src/main/java/org/geektimes/listener/ComponentContextInitializer.java
@@ -0,0 +1,14 @@
+package org.geektimes.listener;
+
+import javax.servlet.ServletContainerInitializer;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import java.util.Set;
+
+public class ComponentContextInitializer implements ServletContainerInitializer {
+    @Override
+    public void onStartup(Set<Class<?>> c, ServletContext servletContext) throws ServletException {
+        // 增加 ServletContextListener
+        servletContext.addListener(ComponentContextInitializerListener.class);
+    }
+}
